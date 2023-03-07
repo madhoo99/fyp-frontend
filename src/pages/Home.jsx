a49b287a75ccff272fba28b5e467c819f80dfa6a
OzairHasan <e0407597@u.nus.edu>
2023-03-07 22:53:09 +0800

    try with just box and text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
 
     return <div>
         <h1>You can customize the lighting and sound as you see fit.</h1>
-        <HomeButton onClickFunc={onButtonClick} endRoute="/Lights" text="Lights" imageSrc="light-png.png" imageAlt="templogo" width={100} height={100}/>
-        <HomeButton onClickFunc={onButtonClick} endRoute="/Sounds" text="Sounds" imageSrc="sound-png.png" imageAlt="templogo" width={100} height={100}/>
+        {/* <HomeButton onClickFunc={onButtonClick} endRoute="/Lights" text="Lights" imageSrc="light-png.png" imageAlt="templogo" width={100} height={100}/>
+        <HomeButton onClickFunc={onButtonClick} endRoute="/Sounds" text="Sounds" imageSrc="sound-png.png" imageAlt="templogo" width={100} height={100}/> */}
         <DrawingArea />
     </div>;
 }
