e409f770271317a6800162b479c1cdd1e395f3df
OzairHasan <e0407597@u.nus.edu>
2023-03-18 16:44:41 +0800

    limit width of drawing area

--- src/pages/Draw.jsx
+++ src/pages/Draw.jsx
@@ -6,6 +6,8 @@ function Draw() {
 
     const navigate = useNavigate();
 
+    const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
     function onButtonClick(endRoute) {
         navigate(endRoute);
     };
@@ -14,7 +16,7 @@ function Draw() {
         <h1>Draw something that reminds you of your childhood.</h1>
         {/* <HomeButton onClickFunc={onButtonClick} endRoute="/Lights" text="Lights" imageSrc="light-png.png" imageAlt="templogo" width={100} height={100}/>
         <HomeButton onClickFunc={onButtonClick} endRoute="/Sounds" text="Sounds" imageSrc="sound-png.png" imageAlt="templogo" width={100} height={100}/> */}
-        <DrawingArea endRoute={'/drawQR'} height={600} width={400} />
+        <DrawingArea endRoute={'/drawQR'} height={400} width={windowSize.current[0]} />
     </div>;
 }
 
