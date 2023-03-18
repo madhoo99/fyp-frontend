b6d3b4cdd3db2dcf8bdb6b558f11a4bf7ccb0dc5
OzairHasan <e0407597@u.nus.edu>
2023-03-18 16:18:25 +0800

    change height of drawing area

--- src/pages/Draw.jsx
+++ src/pages/Draw.jsx
@@ -14,7 +14,7 @@ function Draw() {
         <h1>Draw something that reminds you of your childhood.</h1>
         {/* <HomeButton onClickFunc={onButtonClick} endRoute="/Lights" text="Lights" imageSrc="light-png.png" imageAlt="templogo" width={100} height={100}/>
         <HomeButton onClickFunc={onButtonClick} endRoute="/Sounds" text="Sounds" imageSrc="sound-png.png" imageAlt="templogo" width={100} height={100}/> */}
-        <DrawingArea endRoute={'/drawQR'} height={400} width={400} />
+        <DrawingArea endRoute={'/drawQR'} height={800} width={400} />
     </div>;
 }
 
