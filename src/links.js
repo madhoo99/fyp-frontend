3344e76a86c6d811e28ec04f09cd4815c8752646
OzairHasan <e0407597@u.nus.edu>
2023-04-04 18:41:11 +0800

    modify backend link

--- src/links.js
+++ src/links.js
@@ -1,3 +1,4 @@
-const BACKEND_LINK = 'http://localhost:8000';
+// const BACKEND_LINK = 'http://localhost:8000';
+const BACKEND_LINK = 'https://borderless-backend.herokuapp.com';
 
 export default BACKEND_LINK;
