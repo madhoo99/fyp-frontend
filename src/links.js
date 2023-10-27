75b05b681883a4fed8230f72220afd7d7b3accc4
Madhoosudhan Saravanan <e0407617@u.nus.edu>
2023-10-27 16:56:28 +0800

    added server change

--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
 // const BACKEND_LINK = 'http://localhost:8000';
-const BACKEND_LINK = 'https://borderless-backend.herokuapp.com';
+const BACKEND_LINK = 'https://fyp-backend-be46dfd2133e.herokuapp.com';
 
 export default BACKEND_LINK;
